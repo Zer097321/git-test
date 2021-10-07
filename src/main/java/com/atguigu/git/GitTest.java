e6530455e785244351e2d65e8a8ed0fd13ce9e17
Ice <Ice@123.com>
2021-10-07 22:05:43 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello World");
         System.out.println("Git Hello");
+        System.out.println("World Hello");
     }
 }
